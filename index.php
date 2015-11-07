515172eebff87aaf889ea393094c6bf73d1f3382
derekraymorgan <derek.ray.morgan@gmail.com>
2015-11-07 12:22:27 -0800

    Initial Commit. Using Bootstrap 3 and PHP with no PHP framework.

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<meta name="google-site-verification" content="FAFmCgvnSep5s_mzYMANvyELMKnjwi19jKSxbtxCb-s" />
+
+	<title>UNR ACM</title>
+
+
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Tangerine">
+	<link href='https://fonts.googleapis.com/css?family=Vollkorn:400,700' rel='stylesheet' type='text/css'>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<!-- Optional theme
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    -->
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+
+</head>
+
+<nav class="navbar navbar-default">
+
+	<div class="container">
+
+		<div class="navbar-header">
+
+			<a href="/" class="navbar-brand">
+				UNR ACM
+			</a>
+
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapsemenu">
+
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+
+			</button>
+
+		</div>
+
+		<div class="collapse navbar-collapse" id="collapsemenu">
+
+			<ul class="nav navbar-nav">
+
+				<li>
+					<a href="/">Home</a>
+				</li>
+
+				<li>
+					<a href="/events.php">Events</a>
+				</li>
+
+				<li>
+					<a href="/officers.php">Officers</a>
+				</li>
+
+				<li>
+					<a href="/sponsors.php">Sponsors</a>
+				</li>
+
+			</ul>
+
+		</div>
+
+
+
+	</div>
+
+</nav>
+
+<body>
+
+</body>
+
+</html>
