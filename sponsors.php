c59a525f4a54b57566b0b5151cada4b28133b7f8
derekraymorgan <derek.ray.morgan@gmail.com>
2016-02-11 17:23:32 -0800

    Added all sponsor pictures, resized, optimized, and styled images.

--- sponsors.php
+++ sponsors.php
@@ -22,57 +22,66 @@ include 'header.php';
 
 	<div class="row">
 
-		<div class="col-xs-6 thumbnail">
+		<div class="col-xs-12 thumbnail sponsor">
 
-			<a href="signup.php">
+			<a href="http://www.elevate.blue/" target="_blank">
 
-				<img src="images/BecomeAMemberButton.png" alt="">
-
-			</a>
-
-		</div>
-
-		<div class="col-xs-6 thumbnail">
-
-			<a href="signup.php">
-
-				<img src="images/BecomeAMemberButton.png" alt="">
+				<img class="img-responsive sponsor" src="images/sponsors/elevateblue-min.png" alt="">
 
 			</a>
 
 		</div>
 
-		<div class="col-xs-6 thumbnail">
+		<div class="col-xs-12 thumbnail sponsor">
+
+			<a href="http://www.redteal.com/" target="_blank">
+
+				<img class="img-responsive sponsor" src="images/sponsors/redteal-min.png" alt="">
+
+			</a>
+
+		</div>
+
+		<div class="col-xs-12 thumbnail sponsor">
 
 			<a href="signup.php">
 
-				<img src="images/BecomeAMemberButton.png" alt="">
+				<img class="img-responsive sponsor" src="images/sponsors/microsoft-min.png" alt="">
 
 			</a>
 
 		</div>
 
-		/*
-		Trinity applied internet
-		Intuit
-		SNC
-		Google
-		IGT
-		RedTeal
-		Microsoft
-		Marketing Evolution
-		Elevate Blue
-		*/
+		<div class="col-xs-12 thumbnail sponsor">
 
-		<div class="col-xs-6 thumbnail">
+			<a href="www.google.com" target="_blank">
 
-			<a href="signup.php">
-
-				<img src="images/BecomeAMemberButton.png" alt="">
+				<img class="img-responsive sponsor" src="images/sponsors/google-min.jpg" alt="">
 
 			</a>
 
 		</div>
+
+		<div class="col-xs-12 thumbnail sponsor">
+
+			<a href="http://www.intuit.com/" target="_blank">
+
+				<img class="img-responsive sponsor" src="images/sponsors/intuit-min.png" alt="">
+
+			</a>
+
+		</div>
+
+		<div class="col-xs-12 thumbnail">
+
+			<a href="https://trinity-ai.com/" target="_blank">
+
+				<img class="img-responsive sponsor" src="images/sponsors/trinity-min.png" alt="">
+
+			</a>
+
+		</div>
+
 
 	</div>
 
